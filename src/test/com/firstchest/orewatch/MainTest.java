71ea12c0e003350512aad31ba8c68badde90e637
nerdpower <nerdpower47@gmail.com>
2013-09-04 14:38:14 -0600

    Move common testing functionality to higher scope.

--- src/test/com/firstchest/orewatch/MainTest.java
+++ src/test/com/firstchest/orewatch/MainTest.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
@@ -19,7 +20,24 @@ import com.firstchest.orewatch.Main;
 
 @RunWith( PowerMockRunner.class )
 @PrepareForTest( BlockBreakEvent.class )
-public class MainTest {
+public class MainTest
+{
+	private BlockBreakEvent _mockEvent;
+	private Player _mockPlayer;
+	private String _givenName;
+	
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		_givenName = UUID.randomUUID().toString();
+
+		_mockEvent = PowerMockito.mock( BlockBreakEvent.class );
+		_mockPlayer = mock( Player.class );
+		
+		when( _mockPlayer.getName() ).thenReturn( _givenName );
+		when( _mockEvent.getPlayer() ).thenReturn( _mockPlayer );		
+	}
 
 	
 	/**
@@ -29,19 +47,13 @@ public class MainTest {
 	@Test
 	public void testBlockBreak_AddPlayerToLog()
 	{
-		BlockBreakEvent mockEvent = PowerMockito.mock( BlockBreakEvent.class );
-		Player mockPlayer = mock( Player.class );
+		String expected = _givenName.toUpperCase();
 
-		String given = UUID.randomUUID().toString();
-		when( mockPlayer.getName() ).thenReturn( given );
-		when( mockEvent.getPlayer() ).thenReturn( mockPlayer );
-		
 		// MUT
 		Main main = new Main();
-		main.BlockBreak( mockEvent );
+		main.BlockBreak( _mockEvent );
 		
 		// verify the player is contained in the hashmap log
-		String expected = given.toUpperCase();
 		assertTrue( "Player name not found in hashmap.",
 				main.playerOreLog.containsKey( expected ));
 	}
