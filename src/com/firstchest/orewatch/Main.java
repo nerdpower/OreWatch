2d467c176dce09834348843e9bfabe7f8a0614f0
nerdpower <nerdpower47@gmail.com>
2013-09-05 10:13:16 -0600

    Hook up the block break event handler in main.

--- src/com/firstchest/orewatch/Main.java
+++ src/com/firstchest/orewatch/Main.java
@@ -1,9 +1,39 @@
 package com.firstchest.orewatch;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class Main extends JavaPlugin implements Listener 
 {
+	private BlockBreakHandler _blockBreakHandler;
+	
+	
+	/**
+	 * Default constructor that initializes external dependencies
+	 */
+	public Main()
+	{
+		_blockBreakHandler = new BlockBreakHandler();
+	}
+	
+	
+	/**
+	 * Constructor for testing that implements poor man's dependency injection.
+	 * @param blockBreakHandler
+	 */
+	public Main( BlockBreakHandler blockBreakHandler )
+	{
+		_blockBreakHandler = blockBreakHandler;
+	}
+	
+	
+	@EventHandler( priority = EventPriority.MONITOR )
+	public void BlockBreak( BlockBreakEvent event )
+	{
+		_blockBreakHandler.BlockBreak( event );
+	}
 }
--- src/test/com/firstchest/orewatch/MainTest.java
+++ src/test/com/firstchest/orewatch/MainTest.java
@@ -1,9 +1,15 @@
 package test.com.firstchest.orewatch;
 
+import static org.mockito.Mockito.*;
+
+import org.bukkit.event.block.BlockBreakEvent;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.firstchest.orewatch.BlockBreakHandler;
 import com.firstchest.orewatch.Main;
 
 
@@ -11,4 +17,20 @@ import com.firstchest.orewatch.Main;
 @PrepareForTest( Main.class )
 public class MainTest
 {
+	/**
+	 * Test that the BlockBreak event handler calls the handler class.
+	 */
+	@Test
+	public void BlockBreakEventHookedUp()
+	{	
+		BlockBreakHandler mockHandler = mock( BlockBreakHandler.class );
+		BlockBreakEvent mockEvent = PowerMockito.mock( BlockBreakEvent.class );
+
+		// mut
+		Main main = new Main( mockHandler );
+		main.BlockBreak( mockEvent );
+		
+		// verify the handler class was called
+		verify( mockHandler ).BlockBreak( mockEvent );
+	}
 }
